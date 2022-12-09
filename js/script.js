f1ddcd68f8b716f8a0b7697cbcfa8556458a7367
Sofia <lazareva.1203@gmail.com>
2022-12-09 09:48:23 +0200

    adding other tasks

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function rightScroll() {
+    const right = document.querySelector(".scroller");
+    right.scrollBy(300, 0);
+}
+function leftScroll() {
+    const left = document.querySelector(".scroller");
+    left.scrollBy(-300, 0);
+}
